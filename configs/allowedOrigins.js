42a8d315438f177a82acbcfcabfb75f7176206a7
Vulpes231 <whykayshot@gmail.com>
2024-12-10 16:26:54 +0100

    allowed

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5000",
   "https://financehedge.org",
+  "https://rpc-admin.netlify.app",
 ];
 
 module.exports = { allowedOrigins };
